f66fe502fad0639cfc34416460ff82d560e165a9
Yeimer Molina <yeimer.molina@gmail.com>
2019-03-18 20:53:56 -0500

    Add Heroe Card component

---
+++ src/components/HeroeCard.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+const HeroeCard = ({ heroe }) => (
+    <View style={styles.cardContainer}>
+        <View style={styles.imageContainer}>
+            <Image 
+                source={
+                    { uri: heroe.thumbnail.path + "." + heroe.thumbnail.extension }
+                } 
+                style={styles.heroeImage}
+            />
+        </View>
+        <View style={styles.heroNameContainer}>
+            <Text style={styles.heroName}>{heroe.name}</Text>
+        </View>
+        <View style={styles.heroDescriptionContainer}>
+            <Text style={styles.heroDescription}>{heroe.description}</Text>
+        </View>
+    </View>
+)
+
+const styles = StyleSheet.create({
+    cardContainer: {
+        paddingBottom: 10,
+        borderBottomColor: '#f1f1f1',
+        borderBottomWidth: 1,
+        backgroundColor: 'black'
+    },
+    imageContainer: {
+        width: "100%",
+        height: 300
+    },
+    heroeImage: {
+        width: "100%",
+        height: "100%"
+    },
+    heroNameContainer: {
+        padding: 5,
+        backgroundColor: 'black',
+        alignItems: 'center'
+    },
+    heroName: {
+        fontSize: 24,
+        textAlign: 'center',
+        color: 'white'
+    },
+    heroDescriptionContainer: {
+        padding: 5
+    },
+    heroDescription: {
+        fontSize: 20,
+        color: 'white',
+        textAlign: 'justify'
+    }
+})
+
+export  { HeroeCard };
