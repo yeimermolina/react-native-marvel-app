2156c75e311074934282b5e107547ce8ef84f155
Yeimer Molina <yeimer.molina@gmail.com>
2019-03-18 20:56:05 -0500

    Fetch and display heroes data on heroes screen

--- src/screens/Heroes/index.js
+++ src/screens/Heroes/index.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { FlatList, ScrollView } from 'react-native';
+import { HeroeCard } from '../../components';
+
+import { getHeroes } from '../../store/actions';
 
 class Heroes extends Component {
+  renderHeroe = (heroe) => {
+    return <HeroeCard heroe={heroe.item} />
+  }
+
+  componentDidMount() {
+    this.props.getHeroes();
+  }
+
   render() {
     return (
-      <View>
-        <Text>HEROES SCREEN</Text>
-      </View>
+      <ScrollView>
+        <FlatList
+          data={this.props.heroes}
+          renderItem={this.renderHeroe.bind(this)}
+          keyExtractor={(heroe) => heroe.id.toString()}
+        />
+      </ScrollView>
     )
   }
 }
 
-export default Heroes;
+const mapStateToProps = ({ heroes }) => ({
+  heroes: heroes.heroesList
+})
+
+export default connect(mapStateToProps, { getHeroes })(Heroes);
