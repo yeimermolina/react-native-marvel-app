5173ccc6b2f404d310a98723fd91be1ab7fa5632
Yeimer Molina <yeimer.molina@gmail.com>
2019-03-20 12:10:48 -0500

    Add Separator component

---
+++ src/components/Separator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Separator = () => (
+    <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "f1f1f1"
+        }}
+    />
+);
+
+export { Separator };
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export * from './HeroCard';
 export * from './HeroItem';
+export * from './Separator';
